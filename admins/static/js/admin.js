1727da8b2677fe7299aeb442df677d22008c3da6
jianyong0929 <143489414+jianyong0929@users.noreply.github.com>
2023-09-23 16:25:38 +0800

    Update admin.js

--- admins/static/js/admin.js
+++ admins/static/js/admin.js
@@ -1,24 +1,34 @@
-document.getElementById('Reject').onclick = function() {
-    console.log('Reject button clicked'); // Add this line for testing
+// Assuming each row has a reject button with a class 'reject'
+let rejectButtons = document.querySelectorAll('.reject');
 
-    // Send a POST request to the server to delete the company
-    fetch('/delete_company', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: 'company_id=' + registration.companyID,
-    })
-    .then(function(response) {
-        if (response.ok) {
-            // Add your approval logic here
-            alert('Rejected ' + registration.companyName);
-        } else {
-            // Handle errors or display an error message
-            console.error('Error deleting company');
-        }
-    })
-    .catch(function(error) {
-        console.error('Error:', error);
+rejectButtons.forEach(function(button) {
+    button.addEventListener('click', function(e) {
+        // Get the company ID associated with this button's row
+        let companyID = e.target.dataset.companyID;
+
+        // Send a POST request to the server to delete the company
+        fetch('/delete_company', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: 'company_id=' + companyID,
+        })
+        .then(function(response) {
+            if (response.ok) {
+                // Add your approval logic here
+                alert('Rejected company with ID: ' + companyID);
+                
+                // Remove the row from the table
+                let row = e.target.closest('tr');
+                row.parentNode.removeChild(row);
+            } else {
+                // Handle errors or display an error message
+                console.error('Error deleting company');
+            }
+        })
+        .catch(function(error) {
+            console.error('Error:', error);
+        });
     });
-};
+});
